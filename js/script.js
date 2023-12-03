3a011e2cf90fa1613a43eaeb0faa92093b6905b8
alstdying <alstdying@gmail.com>
2023-12-03 19:52:12 -0300

    menu-ativo

---
+++ js/script.js
@@ -0,0 +1,12 @@
+//ativar links da nav
+const links = document.querySelectorAll(".header-menu a");
+
+function ativarLink(link){
+    const url = location.href;
+    const href = link.href;
+    if(url.includes(href)){
+        link.classList.add("ativo");
+    }
+};
+
+links.forEach(ativarLink);
