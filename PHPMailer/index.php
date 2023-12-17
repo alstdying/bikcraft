a46e2717f61425bd92533790fcce4e5a30f85215
alstdying <alstdying@gmail.com>
2023-12-17 18:57:16 -0300

    enviar-email-ok

---
+++ PHPMailer/index.php
@@ -0,0 +1,34 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require 'vendor/autoload.php';
+
+$mail = new PHPMailer(true);
+
+try {
+    
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+    $mail->isSMTP();
+    $mail->Host       = 'smtp.gmail.com';
+    $mail->SMTPAuth   = true;
+    $mail->Username   = 'alstdying@gmail.com';
+    $mail->Password   = '93S@o4loGg0!#gM2';
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('alstdying@gmail.com', 'Mailer');
+    $mail->addAddress('alinelimapd@gmail.com', 'teste');
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
--- enviar.php
+++ enviar.php
@@ -1,58 +1,65 @@
 <?php
-    $email = "";
-    $senha = "";
+$email = 'alstdying@gmail.com';
+$senha = 'ocmgcdbtksgjcavc';
 
-    $smtp = "smtp.titan.email";
-    $porta = 465;
+$smtp = "smtp.gmail.com";
+$porta = 465;
 
-    $body = "";
+$body = "";
 
-    foreach($_POST as $key => $value){
-        $body .= filter_var($key, FILTER_SANITIZE_STRING)
-        . ": " 
-        . filter_var($value, FILTER_SANITIZE_STRING) . "<br>";
-    }
+foreach ($_POST as $key => $value) {
+  $body .= filter_var($key, FILTER_SANITIZE_STRING)
+    . ": "
+    . filter_var($value, FILTER_SANITIZE_STRING) . "<br>";
+}
 
-    $email_contato = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
-    $email_valid = filter_var($email_contato, FILTER_VALIDATE_EMAIL);
+$email_contato = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
+$email_valid = filter_var($email_contato, FILTER_VALIDATE_EMAIL);
 
-    if(!$email_valid) {
-        throw new Exception('Email inválido');
-    }
+if (!$email_valid) {
+  throw new Exception('Email inválido');
+}
 
-    use PHPMailer\PHPMailer\PHPMailer;
-    use PHPMailer\PHPMailer\SMTP;
-    use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
-    require "./PHPMailer/src/Exception.php";
-    require "./PHPMailer/src/PHPMailer.php";
-    require "./PHPMailer/src/SMTP.php";
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
 
-    $mail = new PHPMailer(true);
+$mail = new PHPMailer(true);
 
-    try {
-        // $mail->SMTPDebug = SMTP::DEBUG_SERVER;
-        $mail->isSMTP();
-        $mail->Host       = $smtp;
-        $mail->SMTPAuth   = true;
-        $mail->Username   = $email;
-        $mail->Password   = $senha;
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
-        $mail->Port       = $porta;
-        $mail->CharSet    = "utf-8";
-      
-        //Recipients
-        $mail->setFrom($email, "Formulário");
-        $mail->addAddress($email, "Formulário");
-        $mail->addReplyTo($email_contato);
-      
-        $mail->isHTML(true);
-        $mail->Subject = "Formulário Email";
-        $mail->Body    = $body;
-      
-        $mail->send();
-        echo "<h1>Mensagem enviada.</h1>";
-      } catch (Exception $e) {
-        echo "<h1>Erro, mensagem não enviada.</h1>";
-      }
-?>
+try {
+  $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+  $mail->isSMTP();
+  $mail->Host       = $smtp;
+  $mail->SMTPAuth   = true;
+  $mail->Username   = $email;
+  $mail->Password   = $senha;
+  $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+  $mail->Port       = $porta;
+  $mail->CharSet    = "utf-8";
+
+  $mail->SMTPOptions = array(
+    'ssl' => array(
+      'verify_peer' => false,
+      'verify_peer_name' => false,
+      'allow_self_signed' => true
+    )
+  );
+
+  //Recipients
+  $mail->setFrom($email, "Bikcraft");
+  $mail->addAddress($email_valid);
+  $mail->addReplyTo($email);
+
+  $mail->isHTML(true);
+  $mail->Subject = "Formulário - Bikcraft";
+  $mail->Body    = $body;
+
+  $mail->send();
+  echo "<h1>Mensagem enviada.</h1>";
+} catch (Exception $e) {
+  echo "<h1>Erro, mensagem não enviada.</h1>";
+}
